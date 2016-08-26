acabc5ad7ad05735fbb4301bbf8749af10341808
Eiskrieger <Eiskrieger@EISKRIEGER-PC>
2016-08-26 13:19:17 -0600

    Add project files.

---
+++ UninformedSearch/Search.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int SIZE = 3;
+	string file_name = "";
+	cout << "Enter a file name to read a puzzle from." << endl;
+	if (!(cin >> file_name))
+	{
+		cout << "\nThe file name was invalid." << endl;
+		return 1;
+	}
+
+	ifstream in(file_name);
+	if (!in)
+	{
+		cout << "The file was not found or could not be opened." << endl;
+		return 1;
+	}
+
+	// Creates a 3x3 array to hold the puzzle
+	int puzzle[SIZE][SIZE];
+	// The location of the zero (i.e. empty) tile.
+	int zero[2];
+	int count;
+	while (in && count <= 8)
+	{
+		char temp;
+		if (in >> temp)
+		{
+			if (temp == '_')
+			{
+				puzzle[count / SIZE][count % SIZE] = 0;
+				zero[0] = count / SIZE;
+				zero[1] = count++ % SIZE;
+			}
+			else
+				puzzle[count / SIZE][count++ % SIZE] = (int)temp;
+		}
+	};
+
+	int solution[SIZE][SIZE] = {
+		{ 1, 2, 3 }, 
+		{ 4, 5, 6 }, 
+		{ 7, 8, 0 } 
+	};
+
+
+	
+
+	
+
+
+
+	return 0;
+}
