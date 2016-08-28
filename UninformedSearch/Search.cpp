5b0b0dd65c3471219ff1cc4ca18c97106e326797
Brad Allred <ballred@purch.com>
2016-08-27 20:40:06 -0600

    Go back to guaranteed contiguous memory layout
    must use a simple custom struct instead of std::array

--- UninformedSearch/Search.cpp
+++ UninformedSearch/Search.cpp
@@ -12,7 +12,46 @@ using namespace std;
 template<size_t N>
 class Puzzle {
 public:
-	using PuzzleState = array<array<int, N>, N>;
+	struct PuzzleState {
+		static constexpr auto n = N;
+		static constexpr auto size = N*N;
+
+		int state[N][N] = {};
+
+		decltype(state[N])& operator[](size_t i)
+		{
+			return state[i];
+		}
+
+		bool operator==(const PuzzleState& rhs) const
+		{
+			for (auto i = 0; i < size; ++i)
+			{
+				if (*nth(i) != rhs.nth(i)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		using iterator = decltype(&state[0][0]);
+
+		// providing iterators so we can easily navigate the 2d array as a flat structure
+		iterator begin()
+		{
+			return &state[0][0];
+		}
+
+		iterator end()
+		{
+			return &state[N-1][N-1];
+		}
+
+		iterator nth(size_t n)
+		{
+			return &state[n / N][n % N];
+		}
+	};
 
 private:
 	PuzzleState state;
@@ -30,19 +69,18 @@ public:
 	// we cant actually use this for anything but testing
 	bool HasSolution()
 	{
-		int inversions = state[0][0] - 1;
-		const auto size = N*N;
-		for (auto i = 1; i < size - 1; ++i)
+		int inversions = *state.begin() - 1;
+		for (auto i = 1; i < state.size - 1; ++i)
 		{
-			const auto cell = &state[i / N][i % N];
+			const auto cell = state.nth(i);
 			const auto val = *cell;
 
 			// no tile is smaller than 1 (since blank tile is *always* ignored in this calculation
 			if (val > 1) {
-				size_t matches = count_if(cell+1, &state[N-1][N-1], [&val](const int& v) {
+				size_t matches = count_if(cell+1, state.end(), [&val](const int& v) {
 					return v && v < val;
 				});
-				assert(matches < size - i);
+				assert(matches < state.size - i);
 				inversions += matches;
 			}
 		}
@@ -108,7 +146,7 @@ int main()
 
 	// Creates a 3x3 array to hold the puzzle
 	Puzzle8::PuzzleState state;
-	auto size = state.size();
+	auto size = state.n;
 	// The location of the zero (i.e. empty) tile.
 	int zero[2];
 	int count = 0;
@@ -131,7 +169,7 @@ int main()
 		}
 	};
 
-	Puzzle8::PuzzleState goal = {{
+	Puzzle8::PuzzleState goal {{
 		{ 1, 2, 3 },
 		{ 4, 5, 6 },
 		{ 7, 8, 0 }
