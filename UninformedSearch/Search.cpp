cc320fc9d2bf13c928520ce03c941a8733e97b57
Brad Allred <ballred@purch.com>
2016-08-27 18:49:59 -0600

    creating scafolding for implemnting puzzle solutions
    I changed from C array to C++ array for the state so that we can more easily compare them and manipulate them

--- UninformedSearch/Search.cpp
+++ UninformedSearch/Search.cpp
@@ -1,33 +1,45 @@
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <unordered_set>
+#include <queue>
 
 using namespace std;
 
 template<size_t N>
-struct Puzzle {
-	int grid[N][N] = {};
+class Puzzle {
+public:
+	using PuzzleState = array<array<int, N>, N>;
 
-	decltype(grid[N])& operator[](size_t i)
+private:
+	PuzzleState state;
+	PuzzleState goal;
+
+public:
+	Puzzle(PuzzleState initial, PuzzleState goal)
+	: state(initial), goal(goal) {}
+
+	auto operator[](size_t i) -> decltype(state[i])
 	{
-		return grid[i];
+		return state[i];
 	}
 
 	// we cant actually use this for anything but testing
 	bool HasSolution()
 	{
-		int inversions = grid[0][0] - 1;
+		int inversions = state[0][0] - 1;
 		const auto size = N*N;
 		for (auto i = 1; i < size - 1; ++i)
 		{
-			const auto cell = &grid[i / N][i % N];
+			const auto cell = &state[i / N][i % N];
 			const auto val = *cell;
 
 			// no tile is smaller than 1 (since blank tile is *always* ignored in this calculation
 			if (val > 1) {
-				size_t matches = count_if(cell+1, &grid[N-1][N-1], [&val](const int& v) {
+				size_t matches = count_if(cell+1, &state[N-1][N-1], [&val](const int& v) {
 					return v && v < val;
 				});
 				assert(matches < size - i);
@@ -36,13 +48,49 @@ struct Puzzle {
 		}
 		return inversions % 2 == 0;
 	}
+
+	bool Solve(/* probably need to pass a strategy */)
+	{
+		// not sure about any of this yet
+
+		struct Node {
+
+		};
+
+		queue<Node> frontier;
+		unordered_set<Node> explored;
+	}
+
+	bool IsSolved()
+	{
+		return state == goal;
+	}
+
+	bool MoveUp()
+	{
+
+	}
+
+	bool MoveDown()
+	{
+
+	}
+
+	bool MoveLeft()
+	{
+
+	}
+
+	bool MoveRight()
+	{
+
+	}
 };
 
 using Puzzle8 = Puzzle<3>;
 
 int main()
 {
-	const int SIZE = 3;
 	string file_name = "";
 	cout << "Enter a file name to read a puzzle from." << endl;
 	if (!(cin >> file_name))
@@ -59,7 +107,8 @@ int main()
 	}
 
 	// Creates a 3x3 array to hold the puzzle
-	int puzzle[SIZE][SIZE];
+	Puzzle8::PuzzleState state;
+	auto size = state.size();
 	// The location of the zero (i.e. empty) tile.
 	int zero[2];
 	int count = 0;
@@ -70,28 +119,26 @@ int main()
 		{
 			if (temp == '_')
 			{
-				puzzle[count / SIZE][count % SIZE] = 0;
-				zero[0] = count / SIZE;
-				zero[1] = count++ % SIZE;
+				state[count / size][count % size] = 0;
+				zero[0] = count / size;
+				zero[1] = count++ % size;
 			}
 			else
 			{
-				puzzle[count / SIZE][count % SIZE] = temp - '0';
+				state[count / size][count % size] = temp - '0';
 				++count;
 			}
 		}
 	};
 
-	int solution[SIZE][SIZE] = {
-		{ 1, 2, 3 }, 
-		{ 4, 5, 6 }, 
-		{ 7, 8, 0 } 
-	};
+	Puzzle8::PuzzleState goal = {{
+		{ 1, 2, 3 },
+		{ 4, 5, 6 },
+		{ 7, 8, 0 }
+	}};
+	Puzzle8 puzzle(state, goal);
 
-
-	
-
-	
+	cout << puzzle.HasSolution() << endl;
 
 
 
